f735f994c0a7640537b52128b6f145205f0808f9
adib653 <136230467+adib653@users.noreply.github.com>
2023-07-01 17:54:57 +0700

    Add files via upload

---
+++ Module Ke 4/Code/Belajar/coba.php
@@ -0,0 +1,7 @@
+<?php
+echo "Saya sedang belajar PHP di kelas Pemrograman Web Universitas Paramadina";
+echo "<br>";
+echo "Muhammad Adib Garribaldi 120103030";
+echo "<br>";
+echo "Hello World...";
+?>
---
+++ Module Ke 4/Code/Belajar/operator2.php
@@ -0,0 +1,11 @@
+<?
+$a = 5;
+$b = 4;
+
+eccho "$a == $b : ". ($a == $b);
+eccho "<br>$a != $b : ". ($a != $b);
+eccho "<br>$a > $b : ". ($a > $b);
+eccho "<br>$a < $b : ". ($a < $b);
+eccho "<br>($a == $b) && ($a > $b) : ".(($a != $b) && ($a > $b))
+eccho "<br>($a == $b) || ($a > $b) : ".(($a != $b) || ($a > $b))
+?>
